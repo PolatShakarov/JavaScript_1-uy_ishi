0e5cdb52e43ab132eb988f022ef613024687ae0a
Polat29 <smartrobot299@gmail.com>
2022-07-04 19:23:32 +0500

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var UserName = prompt("Ismingiz...")
+var Surname = prompt("Familiyangiz...")
+var Nationality = prompt("Millatingiz...")
+var Place_of_birth = prompt("tug'ilgan joyingiz...")
+var UserData = prompt("tug'ilgan sanangiz...")
+var Nationality = prompt("Fuqaroligi")
+var UserInformation = prompt("ma'lumotingiz...")
+var Student = prompt("O'qish joyingiz", "Ixtiyoriy")
+var Workplace = prompt("Ish joyingiz:")
+var Condition = prompt("holati", "Ixtiyoriy")
+
+alert(`Ism: ${UserName} \nFamiliya: ${Surname} \nMillati: ${Nationality} \nTug'ilgan joyi: ${Place_of_birth} \nTug'ilgan sana ${UserData} \nFuqaroligi ${Nationality} \nma'lumoti: ${UserInformation} \nO'qish joyi: ${Student} \nIsh joyi: ${Workplace} \nHolati: ${Condition} \n\n\n Click "OK"\n`)
+
+console.log(`Ism: ${UserName} \nFamiliya: ${Surname} \nMillati: ${Nationality} \nTug'ilgan joyi: ${Place_of_birth} \nTug'ilgan sana ${UserData} \nFuqaroligi ${Nationality} \nma'lumoti: ${UserInformation} \nO'qish joyi: ${Student} \nIsh joyi: ${Workplace} \nHolati: ${Condition}`)
